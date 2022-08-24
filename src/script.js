764a64ecf4ead87bd65fd163c84480ee2506d524
Jacopo <jacopo.v@pm.me>
2022-08-24 16:54:15 +0100

    feat: new displacement map

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import "./style.css"
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 import gsap from "gsap"
+import GUI from "lil-gui"
 
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 gsap.registerPlugin(ScrollTrigger)
@@ -10,12 +11,10 @@ gsap.registerPlugin(ScrollTrigger)
 const canvas = document.querySelector("canvas.webgl")
 
 // Loader
-const TEXTURE_PATH =
-  "https://res.cloudinary.com/dg5nsedzw/image/upload/v1641657168/blog/vaporwave-threejs-textures/grid.png"
 const textureLoader = new THREE.TextureLoader()
 const gridTexture = textureLoader.load("/texture/grid-texture.png")
-// const terrainTexture = textureLoader.load("/texture/displacementmap.webp");
-const terrainTexture = textureLoader.load("/texture/displacementmap2.png")
+// const terrainTexture = textureLoader.load("/texture/displacementmap2.png")
+const terrainTexture = textureLoader.load("/texture/displacementmap4.png")
 
 // Sizes
 const sizes = {
@@ -30,10 +29,8 @@ const scene = new THREE.Scene()
 
 const planeGeometry = new THREE.PlaneGeometry(1, 2, 24, 24)
 const planeMaterial = new THREE.MeshStandardMaterial({
-  // color: "white",
-  // wireframe: true,
-  // side: THREE.DoubleSide,
   map: gridTexture,
+  // wireframe: true,
   displacementMap: terrainTexture,
   displacementScale: 0.3,
 })
@@ -44,10 +41,16 @@ plane.rotation.x = -Math.PI * 0.5
 plane.position.y = 0.0
 plane.position.z = 0.15
 
-const movePlane = ({ direction }) => {
+const plane2 = new THREE.Mesh(planeGeometry, planeMaterial)
+plane2.rotation.x = -Math.PI * 0.5
+plane2.position.y = 0.0
+plane2.position.z = -1.85 // 0.15 - 2 (the length of the first plane)
+
+const movePlane = ({ isMovingForward }) => {
   const scrollAmout = 0.3
-  const zModifier = direction ? scrollAmout : -1 * scrollAmout
+  const zModifier = isMovingForward ? scrollAmout : -1 * scrollAmout
   gsap.to(plane.position, { z: plane.position.z + zModifier })
+  gsap.to(plane2.position, { z: plane2.position.z + zModifier })
 }
 
 let touchStartClientY = 0
@@ -57,31 +60,30 @@ document.addEventListener("touchstart", ({ changedTouches }) => {
 
 document.addEventListener("touchmove", ({ changedTouches }) => {
   const isScrollingDown = changedTouches[0].clientY > touchStartClientY
-  movePlane({ direction: isScrollingDown })
+  movePlane({ isMovingForward: isScrollingDown })
 })
 
 document.addEventListener("wheel", ({ deltaY }) => {
-  movePlane({ direction: deltaY > 1 })
+  movePlane({ isMovingForward: deltaY > 1 })
 })
 
 scene.add(plane)
+scene.add(plane2)
 
 const ambientLight = new THREE.AmbientLight("#ffffff", 10)
 scene.add(ambientLight)
+
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-// camera.position.x = 0;
-// camera.position.y = 0.25;
-// camera.position.z = 1;
 camera.position.x = 0
-camera.position.y = 0.1
+camera.position.y = 0.2
 camera.position.z = 1.1
 camera.lookAt(plane.position)
 scene.add(camera)
 
-// Controls
-// const controls = new OrbitControls(camera, canvas);
-// controls.enableZoom = false;
+// // Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableZoom = false
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -92,19 +94,16 @@ renderer.setSize(sizes.width, sizes.height)
 
 window.addEventListener("resize", () => {
   sizes.height = window.innerHeight
+  sizes.width = window.innerWidth
 
   // Update camera's aspect ratio and projection matrix
-
   camera.aspect = sizes.width / sizes.height
-
-  // camera.updateProjectionMatrix();
+  camera.updateProjectionMatrix()
 
   // Update renderer
-
   renderer.setSize(sizes.width, sizes.height)
 
   // Note: We set the pixel ratio of the renderer to at most 2
-
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
@@ -116,6 +115,8 @@ const tick = () => {
 
   // Update objects
 
+  // camera.position
+
   // Render
   renderer.render(scene, camera)
 
@@ -124,3 +125,14 @@ const tick = () => {
 }
 
 tick()
+
+// GUI
+
+const gui = new GUI()
+
+gui.add(camera.position, "x").min(-2).max(2).step(0.1)
+gui.add(camera.position, "y").min(-2).max(2).step(0.1)
+gui.add(camera.position, "z").min(-2).max(2).step(0.1)
+// gui.add(plane.rotation, "x").min(-2).max(2).step(0.1)
+// gui.add(plane.rotation, "y").min(-2).max(2).step(0.1)
+// gui.add(plane.rotation, "z").min(-2).max(2).step(0.1)
